5c354289a7d48463ec5878820435089aa69c28bb
Eva O'Brien <evaobrien.contact@gmail.com>
2024-09-28 15:02:41 -0400

    Update cmd.js
    - removed double quotes
    - added newline at end of file

--- cmd.js
+++ cmd.js
@@ -6,6 +6,6 @@ const { resolve } = require('node:path')
 
 const args = [...process.argv.slice(2)]
 
-const directory = (!args[0] || args[0] === "" || args[0].startsWith('-')) ? '.' : resolve(args[0])
+const directory = (!args[0] || args[0] === '' || args[0].startsWith('-')) ? '.' : resolve(args[0])
 
 require('fastify-cli/generate').cli([directory])
